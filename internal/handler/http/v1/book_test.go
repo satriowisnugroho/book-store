dbfdbe74db55cc950fa0d4b7a719ca5f5facd1db
Satrio Wisnugroho <wisnugrohosatrio@gmail.com>
2024-06-06 22:12:55 +0700

    Add ut book handler

---
+++ internal/handler/http/v1/book_test.go
@@ -0,0 +1,53 @@
+package v1_test
+
+import (
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/satriowisnugroho/book-store/internal/entity"
+	httpv1 "github.com/satriowisnugroho/book-store/internal/handler/http/v1"
+	testmock "github.com/satriowisnugroho/book-store/test/mock"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestGetBooks(t *testing.T) {
+	testcases := []struct {
+		name              string
+		uBookErr          error
+		httpStatusCodeRes int
+	}{
+		{
+			name:              "failed to get books",
+			uBookErr:          errors.New("error get books"),
+			httpStatusCodeRes: http.StatusInternalServerError,
+		},
+		{
+			name:              "success",
+			httpStatusCodeRes: http.StatusOK,
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			w := httptest.NewRecorder()
+			ctx, _ := gin.CreateTestContext(w)
+
+			ctx.Request, _ = http.NewRequest("GET", "/books", nil)
+
+			l := &testmock.LoggerInterface{}
+			l.On("Error", mock.Anything, mock.Anything)
+
+			bookUsecase := &testmock.BookUsecaseInterface{}
+			bookUsecase.On("GetBooks", mock.Anything).Return([]*entity.Book{{}}, tc.uBookErr)
+
+			h := &httpv1.BookHandler{l, bookUsecase}
+			h.GetBooks(ctx)
+
+			assert.Equal(t, tc.httpStatusCodeRes, w.Code)
+		})
+	}
+}
