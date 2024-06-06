c20cf3c554a987fa158fd0d227bc2724ba5ac43e
Satrio Wisnugroho <wisnugrohosatrio@gmail.com>
2024-06-06 21:56:12 +0700

    Add ut book repo

---
+++ internal/repository/postgres/book_test.go
@@ -0,0 +1,88 @@
+package postgres_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/jmoiron/sqlx"
+	"github.com/pkg/errors"
+	"github.com/satriowisnugroho/book-store/internal/entity"
+	"github.com/satriowisnugroho/book-store/internal/repository/postgres"
+	"github.com/satriowisnugroho/book-store/test/fixture"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetBooks(t *testing.T) {
+	testcases := []struct {
+		name      string
+		ctx       context.Context
+		fetchErr  error
+		fetchRows []string
+		expected  []*entity.Book
+		wantErr   bool
+	}{
+		{
+			name:    "deadline context",
+			ctx:     fixture.CtxEnded(),
+			wantErr: true,
+		},
+		{
+			name:     "fail fetch query error",
+			ctx:      context.Background(),
+			fetchErr: errors.New("fail fetch"),
+			wantErr:  true,
+		},
+		{
+			name:      "fail fetch return error rows",
+			ctx:       context.Background(),
+			fetchRows: []string{"unknown_column"},
+			wantErr:   true,
+		},
+		{
+			name:      "success",
+			ctx:       context.Background(),
+			fetchRows: postgres.BookColumns,
+			expected:  []*entity.Book{{}},
+			wantErr:   false,
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			db, mock, err := sqlmock.New()
+			if err != nil {
+				t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+			}
+			defer db.Close()
+
+			if tc.fetchErr != nil {
+				mock.ExpectQuery("^SELECT(.+)").WillReturnError(tc.fetchErr)
+			} else {
+				rows := sqlmock.NewRows(tc.fetchRows)
+				if tc.expected != nil {
+					rows = rows.AddRow(
+						tc.expected[0].ID,
+						tc.expected[0].Isbn,
+						tc.expected[0].Title,
+						tc.expected[0].Price,
+						tc.expected[0].CreatedAt,
+						tc.expected[0].UpdatedAt,
+					)
+				} else if len(tc.fetchRows) == 1 {
+					rows = rows.AddRow(1)
+				}
+
+				mock.ExpectQuery("^SELECT(.+)").WillReturnRows(rows)
+			}
+
+			dbx := sqlx.NewDb(db, "mock")
+			repo := postgres.NewBookRepository(dbx)
+			result, err := repo.GetBooks(tc.ctx)
+			assert.Equal(t, tc.wantErr, err != nil, err)
+			if !tc.wantErr {
+				assert.EqualValues(t, tc.expected, result)
+			}
+		})
+	}
+}
