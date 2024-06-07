7fd4e429577a3fa69b8121a4016289622e62c0f1
Satrio Wisnugroho <wisnugrohosatrio@gmail.com>
2024-06-07 23:05:00 +0700

    Fix serial seed

--- db/seed.sql
+++ db/seed.sql
@@ -10,9 +10,9 @@
 --
 TRUNCATE public.books RESTART IDENTITY CASCADE;
 
-COPY public.books (id, isbn, title, price, created_at, updated_at) FROM stdin;
-1	lorem	ipsum	123	2024-06-06 15:37:14	2024-06-06 15:37:14
-2	ipsum	lorem	123	2024-06-06 15:37:14	2024-06-06 15:37:14
+COPY public.books (isbn, title, price, created_at, updated_at) FROM stdin;
+lorem	ipsum	123	2024-06-06 15:37:14	2024-06-06 15:37:14
+ipsum	lorem	123	2024-06-06 15:37:14	2024-06-06 15:37:14
 \.
 
 --
@@ -21,9 +21,9 @@ COPY public.books (id, isbn, title, price, created_at, updated_at) FROM stdin;
 
 TRUNCATE public.orders RESTART IDENTITY CASCADE;
 
-COPY public.orders (id, user_id, book_id, quantity, price, fee, total_price, created_at, updated_at) FROM stdin;
-1	1	1	4	25000	1000	101000	2024-06-07 22:43:27.750419	2024-06-07 22:43:27.750419
-2	2	2	4	20000	1000	81000	2024-06-07 22:43:27.750419	2024-06-07 22:43:27.750419
+COPY public.orders (user_id, book_id, quantity, price, fee, total_price, created_at, updated_at) FROM stdin;
+1	1	4	25000	1000	101000	2024-06-07 22:43:27.750419	2024-06-07 22:43:27.750419
+2	2	4	20000	1000	81000	2024-06-07 22:43:27.750419	2024-06-07 22:43:27.750419
 \.
 
 --
@@ -32,9 +32,9 @@ COPY public.orders (id, user_id, book_id, quantity, price, fee, total_price, cre
 
 TRUNCATE public.users RESTART IDENTITY CASCADE;
 
-COPY public.users (id, email, fullname, crypted_password, created_at, updated_at) FROM stdin;
-1	foo@gmail.com	bar	qwerty	2024-06-06 15:37:14	2024-06-06 15:37:14
-2	bar@gmail.com	foo	qwerty	2024-06-06 15:37:14	2024-06-06 15:37:14
+COPY public.users (email, fullname, crypted_password, created_at, updated_at) FROM stdin;
+foo@gmail.com	bar	qwerty	2024-06-06 15:37:14	2024-06-06 15:37:14
+bar@gmail.com	foo	qwerty	2024-06-06 15:37:14	2024-06-06 15:37:14
 \.
 
 --
