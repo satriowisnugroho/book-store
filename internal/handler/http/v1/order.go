e91d3a24da553893c7e6e5fd7c38e732e4a6c03e
Satrio Wisnugroho <wisnugrohosatrio@gmail.com>
2024-06-07 22:35:31 +0700

    Create order handler

--- internal/handler/http/v1/order.go
+++ internal/handler/http/v1/order.go
@@ -1,8 +1,10 @@
 package v1
 
 import (
+	"encoding/json"
+
 	"github.com/gin-gonic/gin"
-	_ "github.com/satriowisnugroho/book-store/internal/entity"
+	"github.com/satriowisnugroho/book-store/internal/entity"
 	"github.com/satriowisnugroho/book-store/internal/response"
 	"github.com/satriowisnugroho/book-store/internal/usecase"
 	"github.com/satriowisnugroho/book-store/pkg/logger"
@@ -18,8 +20,41 @@ func newOrderHandler(handler *gin.RouterGroup, l logger.LoggerInterface, bu usec
 
 	h := handler.Group("/orders")
 	{
+		h.POST("/", r.CreateOrder)
 		h.GET("/", r.GetOrderHistory)
 	}
+}
+
+// @Summary     Create an Order
+// @Description An API to create an order
+// @ID          create order
+// @Tags  	    order
+// @Accept      json
+// @Produce     json
+// @Param       request		body		entity.OrderPayload		true		"payload"
+// @Success     200 {object} response.SuccessBody{data=entity.Order,meta=response.MetaInfo}
+// @Failure     404 {object} response.ErrorBody
+// @Failure     422 {object} response.ErrorBody
+// @Failure     500 {object} response.ErrorBody
+// @Router      /orders [post]
+func (h *OrderHandler) CreateOrder(c *gin.Context) {
+	var payload entity.OrderPayload
+	if err := json.NewDecoder(c.Request.Body).Decode(&payload); err != nil {
+		h.Logger.Error(err, "http - v1 - Decode payload")
+		response.Error(c, err)
+
+		return
+	}
+
+	order, err := h.OrderUsecase.CreateOrder(c.Request.Context(), &payload)
+	if err != nil {
+		h.Logger.Error(err, "http - v1 - CreateOrder")
+		response.Error(c, err)
+
+		return
+	}
+
+	response.OK(c, order, "Successfully create an order")
 }
 
 // @Summary     Show History Order
--- internal/handler/http/v1/order_test.go
+++ internal/handler/http/v1/order_test.go
@@ -2,8 +2,10 @@ package v1_test
 
 import (
 	"errors"
+	"io"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/gin-gonic/gin"
@@ -13,6 +15,58 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
+
+func TestCreateOrder(t *testing.T) {
+	testcases := []struct {
+		name              string
+		body              string
+		uOrderRes         *entity.Order
+		uOrderErr         error
+		httpStatusCodeRes int
+	}{
+		{
+			name:              "failed to decode payload",
+			body:              `{failed}`,
+			httpStatusCodeRes: http.StatusInternalServerError,
+		},
+		{
+			name:              "failed to create product",
+			body:              `{}`,
+			uOrderErr:         errors.New("error create product"),
+			httpStatusCodeRes: http.StatusInternalServerError,
+		},
+		{
+			name:              "success",
+			body:              `{}`,
+			uOrderRes:         &entity.Order{},
+			httpStatusCodeRes: http.StatusOK,
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			w := httptest.NewRecorder()
+			ctx, _ := gin.CreateTestContext(w)
+
+			ctx.Request = &http.Request{
+				Header: make(http.Header),
+				Method: "POST",
+				Body:   io.NopCloser(strings.NewReader(tc.body)),
+			}
+
+			l := &testmock.LoggerInterface{}
+			l.On("Error", mock.Anything, mock.Anything)
+
+			productUsecase := &testmock.OrderUsecaseInterface{}
+			productUsecase.On("CreateOrder", mock.Anything, mock.Anything).Return(tc.uOrderRes, tc.uOrderErr)
+
+			h := &httpv1.OrderHandler{l, productUsecase}
+			h.CreateOrder(ctx)
+
+			assert.Equal(t, tc.httpStatusCodeRes, w.Code)
+		})
+	}
+}
 
 func TestGetOrderHistory(t *testing.T) {
 	testcases := []struct {
